4b72f02e3531e5209e91d1eda54cda950b2a11d7
Sahilkalra1996 <skalraa360@gmail.com>
2025-06-09 20:29:34 -0400

    Assesment

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+console.log('JS Loaded');
+
+FirstName= 'Sahil';
+LastName= 'Kalra';
+
+const myfullName=`${FirstName} ${LastName}`;
+
+console.log(myfullName);
+
+StudentNumber='1225040';
+
+const mystudentNumber=`${StudentNumber}`;
+
+console.log(mystudentNumber);
+
+let myelement = document.querySelector("h1");
+console.log(myelement.innerHTML);
+
+myelement.classList.add("primaryHeading");
+console.log(myelement.classList);
+
